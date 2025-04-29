80cf74ec334ebde6b3379958a55486aa4ccf2e7f
qiura122 <emowijaya122@gmail.com>
2025-04-29 13:41:19 +0800

    navigator UI 3

---
+++ lib/UI3/Navigator_push.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Komang Emo'),
+        backgroundColor: Colors.indigoAccent,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Page2()),
+            );
+          },
+          child: const Text('Go To Page 2'),
+        ),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hi, Emo. This is Page 2.'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Home()),
+            );
+          },
+          child: const Text('Back to Home'),
+        ),
+      ),
+    );
+  }
+}
